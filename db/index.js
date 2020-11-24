29618e335f89a0d3e70d4260bed134252f7fdb0a
YoungAncient <68391309+YoungAncient@users.noreply.github.com>
2020-11-23 20:04:36 -0500

    fixed manager name formatting

--- db/index.js
+++ db/index.js
@@ -22,7 +22,7 @@ class DB {
     // get all employees
     getAllEmployees() {
         return this.connection.promise().query(
-            'SELECT employees.id, employees.first_name, employees.last_name, roles.title AS job_title, departments.name AS department, roles.salary, CONCAT(manager.last_name, manager.first_name) AS manager FROM employees LEFT JOIN roles on employees.role_id = roles.id LEFT JOIN departments on roles.department_id = departments.id LEFT JOIN employees manager on manager.id = employees.manager_id;'
+            'SELECT employees.id, employees.first_name, employees.last_name, roles.title AS job_title, departments.name AS department, roles.salary, CONCAT(manager.last_name, ", " , manager.first_name) AS manager FROM employees LEFT JOIN roles on employees.role_id = roles.id LEFT JOIN departments on roles.department_id = departments.id LEFT JOIN employees manager on manager.id = employees.manager_id;'
         );
     };
 
